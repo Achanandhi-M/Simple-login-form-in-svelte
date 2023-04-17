b1290677f48e39440c253a6f8b13d08dfe23c0da
Achanandhi24 <achanandhi.m@gmail.com>
2023-04-17 18:34:24 +0530

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,65 @@
+<script>
+
+    let text;
+    let email;
+    let isDisplay=false;
+    function handleInput(){
+        isDisplay=false;
+    }
+    function display(){
+    if(text&&email){
+        console.log('Name:', text);
+            console.log('Email:', email);
+            isDisplay = true;
+    }
+    }
+</script>
+<style>
+h1{
+    text-align:center;
+	font-family:'Rubik', sans-serif;
+
+}
+form{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+label{
+	font-size:18px;
+	font-family:'Rubik', sans-serif;
+	font-weight: bold;
+}
+input{
+    color:#432000;
+    background-color:#DCE1EB;
+    text-align:center;
+    border-radius:8px;
+    padding:15px;
+    border:0;
+    font-size:20px;
+    font-family:'Rubik', sans-serif;
+    margin:10px 0;
+}
+button{
+	color:#FDFDFD;
+    background-color:#AC485A;
+    text-align:center;
+    border-radius:8px;
+    padding:15px;
+    border:0;
+    font-size:20px;
+    font-family:'Rubik', sans-serif
+}
+</style>
+<h1>Login</h1>
+<form>
+    <label for="name">Enter your Name:</label><br/>
+    <input type="text" bind:value={text} on:input={handleInput}/><br/>
+    <label for="name">Enter your Email id:</label><br/>
+    <input type="email" bind:value={email} on:input={handleInput}/><br/>
+	<button type="button" on:click={display}>Login</button>
+    {#if isDisplay}
+    <h1>Hello {text}! and You logged in as {email}</h1>     
+    {/if}
+</form>
---
+++ src/practise.svelte
@@ -0,0 +1,21 @@
+<script>
+    let inputVal = '';
+  
+    function handleClick() {
+      console.log('Button clicked');
+    }
+  
+    function handleInput() {
+      const button = document.getElementById('myButton');
+      button.click();
+    }
+  </script>
+  
+  <label>
+    Enter a value:
+    <input type="text" bind:value={inputVal} on:input={handleInput} />
+  </label>
+  <br />
+  
+  <button id="myButton" on:click={handleClick}>Click me</button>
+  
